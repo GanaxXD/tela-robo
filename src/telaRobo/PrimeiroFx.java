9245ce27094f4eef2cf6e6437ce7feace40b3f19
Pedro Victor <pedro.mattis.df@gmail.com>
2022-04-25 19:24:11 -0300

    Refatorando o css e a classe java

--- src/telaRobo/PrimeiroFx.java
+++ src/telaRobo/PrimeiroFx.java
@@ -2,6 +2,7 @@ package telaRobo;
 
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,77 +10,118 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Paint;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
 
 public class PrimeiroFx extends Application{
 	
-	public String fonteAssistent = "https://fonts.googleapis.com/css2?family=Assistant:wght@300&display=swap";
-	public String fonteAssistentLabel = "https://fonts.googleapis.com/css2?family=Assistant:wght@700;800&display=swap";
+	public String fonteGeral = "https://fonts.googleapis.com/css2?family=Assistant:wght@300&display=swap";
+	public String fontLabelAssitant = "https://fonts.googleapis.com/css2?family=Assistant:wght@800&display=swap";
 	public Image tjIcon = new Image("images/tjlogo.png");
-	public Image toadaIcon = new Image("images/ToadaLab.png");
+	public Image toadaIcon = new Image("images/toadaIcon.jpg");
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Robô Triador - Tela de Configuração");
+		
+		primaryStage.setTitle("Rôbo Triador - Tela de configuração");
 		primaryStage.centerOnScreen();
 		primaryStage.getIcons().add(tjIcon);
-		
+				
 		//Caixa na vertical (alinha os elementos na vertical)
-		VBox containerExterno = new VBox(50.0);
+		VBox tela = new VBox();
 		VBox containerInterno = new VBox(10.0);
-		HBox perfil = new HBox();
-		HBox tarefas = new HBox();
-		
-		perfil.setMaxSize(600, 120);
-		tarefas.setMaxSize(600, 200);
-		
-		
-		//Botões
-		Button botaoCadastrar = new Button("Cadastar");
-		
-		Label labelPerfil = new Label("Perfil");
-		Label labelTarefas = new Label("Tarefas");
-		Label labelFiltro = new Label("Filtrar?");
-		TextField inputPerfil = new TextField();
-		TextField inputTarefas = new TextField();
+		HBox containerPerfil = new HBox(10.0);
+		HBox containerTarefas = new HBox(10.0);
+		HBox containerFiltrar = new HBox(10.0);
+		GridPane conteudoInterno = new GridPane();
+		conteudoInterno.setHgap(10.0);
 		
 		
-		//Vinculando o botão dentro da caixa inicial
-		containerInterno.getChildren()
-			.addAll(perfil, tarefas);
-		
-		containerExterno.getChildren().add(containerInterno);
-		
-		Stop[] cores = new Stop[] {new Stop(0, Color.WHITE), new Stop(0.5, Color.AQUAMARINE), new Stop(1, Color.CADETBLUE)};
-		LinearGradient gradiente = new LinearGradient(0, 0, 1, 0, true,CycleMethod.NO_CYCLE, cores);
-		
-		//Centralizando
-		containerExterno.setAlignment(Pos.CENTER);
-		//Adicionando cores
-		containerInterno.setBackground(Background.fill(
-				gradiente
-			//Paint.valueOf("linear-gradient(from 0% 0% to 100% 100%, red  -10% , blue 38%,  black 0%)")
-			)
-		);
-		
-		
-		//Definindo espaçamento entre os elementos
-		containerExterno.setSpacing(60);
 		
 		//No Fx, eu tenho dois elementos: O palco (stage) e a Cena (Scene)
 		//Sempre que eu quiser mostrar uma cena (um conjunto organizado de componentes),
 		//eu devo adicionar à cena o mesmo.
 		//O exemplo abaixo faz isso: adiciono a caixa com seus elementos à cena
 		//Depois dos elementos vinculados à cena, eu passo o tamanho da tela (w, h)
-		Scene cena = new Scene(containerExterno, 1300, 760);
+		Scene cena = new Scene(tela, 1000, 650);
+		//e depois, eu vinculo a cena ao palco
+		primaryStage.setScene(cena);
+		
+		//Componentes da página
+		Button botaoCadastrar = new Button("Cadastar");
+		Label perfilLabel = new Label("Perfil");
+		Label tarefasLabel = new Label("Tarefas");
+		Label filtrarLabel = new Label("Filtrar");
+		
+		TextField perfilTextField = new TextField();
+		TextField tarefasTextField = new TextField();
+		TextField filtrarTextField = new TextField();
+		
+	
+		perfilTextField.setAlignment(Pos.CENTER);
+		perfilTextField.setMinWidth(300.0);
+		perfilTextField.setMaxHeight(500.0);
+		
+		//adicionando a um elemento as configuraçãos da classe .container definida no css
+		//Importando o css criado para minha classe:
+		String caminhoCss = getClass()
+				.getResource("/telaRobo/PrimeiroFx.css")
+				.toExternalForm();
+		cena.getStylesheets().add(caminhoCss);
+		tela.getStyleClass().add("container");
+		botaoCadastrar.getStyleClass().addAll("fonteGeral", "botao");
+		botaoCadastrar.getStyleClass().add(fonteGeral);
+		perfilLabel.getStyleClass().add("fontLabelAssitant");
+		perfilLabel.getStyleClass().add(fontLabelAssitant);
+		tarefasLabel.getStyleClass().add("fontLabelAssitant");
+		tarefasLabel.getStyleClass().add(fontLabelAssitant);
+		filtrarLabel.getStyleClass().add("fontLabelAssitant");
+		filtrarLabel.getStyleClass().add(fontLabelAssitant);
+		filtrarTextField.getStyleClass().add("textfield");
+		perfilTextField.getStyleClass().add("textfield");
+		tarefasTextField.getStyleClass().add("textfield");
+		
+		//Vinculando componentes na tela
+		containerPerfil.getChildren().addAll(perfilLabel, perfilTextField);
+		containerTarefas.getChildren().addAll(tarefasLabel, tarefasTextField);
+		containerFiltrar.getChildren().addAll(filtrarLabel, filtrarTextField);
+		containerInterno.getChildren().addAll(containerPerfil, containerTarefas, containerFiltrar, botaoCadastrar);
+		tela.getChildren().add(containerInterno);
+		
+		//--------ESTILIZAÇÃO ---------:
+		containerInterno.setBackground(Background.fill(
+			gradiente(0, 0, 1, 0, new Stop[] {
+				new Stop(0, Color.WHITE),
+				new Stop(0.5, Color.CADETBLUE),
+				new Stop(1, Color.BLUE)
+			})
+			)
+		);
+		
+		containerInterno.setBorder(Border.stroke(Paint.valueOf("#0000FF")));
+		containerInterno.setPrefSize(400.0, 600.0);
+		containerInterno.setMaxSize(500.0, 650.0);
+		containerInterno.setPadding(new Insets(20, 30, 20, 30));
+		containerInterno.setAlignment(Pos.CENTER);
+		containerPerfil.setMaxSize(400.0, 100.0);
+		containerTarefas.setMaxSize(400.0, 100.0);
+		containerFiltrar.setMaxSize(400.0, 100.0);
+		
+		//Centralizando e estilizando a página
+		tela.setAlignment(Pos.CENTER);
+		
+		//Definindo espaçamento entre os elementos
+		tela.setSpacing(30.0);
+		
 		
 		//Adicionando uma função ao pressionar o botão:
 		botaoCadastrar.setOnAction(e ->{
@@ -87,53 +129,18 @@ public class PrimeiroFx extends Application{
 		});
 		
 		
-		//Importando o css criado para minha classe:
-		String caminhoCss = getClass()
-				.getResource("/telaRobo/PrimeiroFx.css")
-				.toExternalForm();
-		
-		cena.getStylesheets().add(caminhoCss);
-		
-		inputPerfil.setMaxSize(450.0, 80.0);
-		inputTarefas.setMaxSize(450.0, 80.0);
-		inputPerfil.getStyleClass().add("input");
-		inputTarefas.getStyleClass().add("input");
-		
-		
-		perfil.getChildren().addAll(labelPerfil, inputPerfil);
-		tarefas.getChildren().addAll(labelTarefas, inputTarefas);
-		
-
-		containerInterno.getStyleClass().add("containerInterno");
-		containerInterno.setAlignment(Pos.CENTER);
-		containerInterno.setMaxWidth(650.0);
-		containerInterno.setMaxHeight(500);
-		containerInterno.setSpacing(20);
-		
-		
-		
-		
-		//adicionando a um elemento as configuraçãos da classe .container definida no css
-		//containerExterno.getStyleClass().add("container");
-		
-		botaoCadastrar.getStyleClass().addAll("fonteGeral", "botao");
-		botaoCadastrar.getStyleClass().add(fonteAssistent);
-		
-		labelPerfil.getStyleClass().add(fonteAssistentLabel);
-		labelTarefas.getStyleClass().add(fonteAssistentLabel);
-		labelFiltro.getStyleClass().add(fonteAssistentLabel);
-		labelPerfil.getStyleClass().add("fonteLabel");
-		labelTarefas.getStyleClass().add("fonteLabel");
-		labelFiltro.getStyleClass().add("fonteLabel");
-		
-		//e depois, eu vinculo a cena ao palco
-		primaryStage.setScene(cena);
-		
 		primaryStage.show();
 	}
 
 	//para rodar:
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	//cria o gradiente
+	public static LinearGradient gradiente(int inicioX, int inicioY, int fimX, int fimY, Stop... cores) {
+		Stop[] coresGradiente = cores;
+		LinearGradient lg = new LinearGradient(inicioX, inicioY, fimX, fimY, true,CycleMethod.REFLECT, coresGradiente);
+		return lg;
 	}
 }
