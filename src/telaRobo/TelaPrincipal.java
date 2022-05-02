dad56dcdedb96a0c1a75bd17fbee16b23100417c
Pedro Victor <pedro.mattis.df@gmail.com>
2022-05-01 22:46:09 -0300

    Correções do layout d tela de seleção de tarefas

--- src/telaRobo/TelaPrincipal.java
+++ src/telaRobo/TelaPrincipal.java
@@ -10,8 +10,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableDoubleValue;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Parent;
@@ -20,6 +26,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
@@ -29,10 +36,12 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -55,7 +64,9 @@ public class TelaPrincipal extends Application {
 	private Stage primaryStage;
 	private Tarefas tarefas = new Tarefas();
 	private List<CheckBox> checkboxes = new ArrayList<CheckBox>();
-
+	int tamanhoLista = 0; //tamanho da lista de tarefas
+	
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
@@ -64,6 +75,7 @@ public class TelaPrincipal extends Application {
 		primaryStage.show();
 	}
 
+	
 	// para rodar:
 	public static void main(String[] args) {
 		launch(args);
@@ -83,11 +95,24 @@ public class TelaPrincipal extends Application {
 		coluna.setFillWidth(true);
 		return coluna;
 	}
+	
+	public ColumnConstraints colunasTarefas(double percentualLargura) {
+		ColumnConstraints coluna = new ColumnConstraints();
+		coluna.setPercentWidth(percentualLargura);
+		coluna.setFillWidth(true);
+		return coluna;
+	}
 
 	// cria as linhas da grid
 	public RowConstraints linhas() {
 		RowConstraints linha = new RowConstraints();
 		linha.setPercentHeight(10);
+		linha.setFillHeight(true);
+		linha.setValignment(VPos.CENTER);
+		return linha;
+	}
+	public RowConstraints linhasTarefas(double percentualAltura) {
+		RowConstraints linha = new RowConstraints();
 		linha.setFillHeight(true);
 		linha.setValignment(VPos.CENTER);
 		return linha;
@@ -158,7 +183,7 @@ public class TelaPrincipal extends Application {
 		}
 
 		navegadorComboBox.getItems().addAll("CHROME", "OPERA", "FIREFOX");
-		
+
 		// adicionando a um elemento as configuraçãos da classe .container definida no
 		// css
 		// Importando o css criado para minha classe:
@@ -166,7 +191,7 @@ public class TelaPrincipal extends Application {
 		telaPrincipal.getStylesheets().add(caminhoCss);
 		tela.getStylesheets().add(caminhoCss);
 		containerInterno.getStylesheets().add(caminhoCss);
-		
+
 		tela.getStyleClass().add("container");
 		botaoCadastrar.getStyleClass().addAll("fonteGeral", "botao");
 		botaoCadastrar.getStyleClass().add(fonteGeral);
@@ -227,14 +252,15 @@ public class TelaPrincipal extends Application {
 		containerInterno.setMaxSize(500.0, 550.0);
 		containerInterno.setPadding(new Insets(20, 30, 20, 30));
 		containerInterno.setAlignment(Pos.CENTER);
-		
+
 		containerPerfil.setMaxSize(400.0, 100.0);
 		containerTarefas.setMaxSize(400.0, 100.0);
 		containerFiltrar.setMaxSize(400.0, 100.0);
-		
-		double tamanhoIdeal = (containerInterno.getMinWidth()/10.0)*6.8;
+
+		double tamanhoIdeal = (containerInterno.getMinWidth() / 10.0) * 6.8;
 		System.out.println(tamanhoIdeal);
-		//Definindo tamanho mínimo dos componentes pelo tamanho do tamanho do "conteudoInterno"
+		// Definindo tamanho mínimo dos componentes pelo tamanho do tamanho do
+		// "conteudoInterno"
 		navegadorComboBox.setMinWidth(tamanhoIdeal);
 		navegadorComboBox.setMaxWidth(tamanhoIdeal);
 		perfilComboBox.setMinWidth(tamanhoIdeal);
@@ -263,9 +289,46 @@ public class TelaPrincipal extends Application {
 
 	// tela de tarefas
 	public void telaTarefas() {
-		FlowPane quadroInterno = new FlowPane(10.0, 5.0);
-		quadroInterno.setMinSize(900.0, 690.0);
-		quadroInterno.setPadding(new Insets(20.0, 10.0, 10.0, 20.0));
+		double larguraDaTela = primaryStage.getWidth();
+		double alturaDaTela = primaryStage.getHeight();
+		
+		System.out.println("ALTURA: "+ alturaDaTela+ " LARGURA: "+larguraDaTela);
+		
+		GridPane quadroDeTarefas = new GridPane();
+		//quadroDeTarefas.setLayoutY(0.0);
+		quadroDeTarefas.setPadding(new Insets(50.0, 10.0, 10.0, 5.0));
+		quadroDeTarefas.setAccessibleHelp("Quadro de tarefas");
+		quadroDeTarefas.setAccessibleRoleDescription("Quadro de tarefas.");
+		quadroDeTarefas
+				.setAccessibleText("Clique na tarefa para selecioná-la. Você pode selecionar mais de uma tarefa.");
+		quadroDeTarefas.setAlignment(Pos.CENTER);
+		quadroDeTarefas.setMinSize(900.0, 690.0);
+		
+		//quadroDeTarefas.prefWidthProperty().bind(ObservableValue<Number> primaryStage.getWidth()); 
+		//quadroDeTarefas.prefHeightProperty().bind(alturaDaTela);
+		
+		quadroDeTarefas.getRowConstraints().add(linhasTarefas(30));
+		CheckBox selecionarTudo = new CheckBox("Selecionar Todas as Tarefas");
+		CheckBox retirarSelecaoTudo = new CheckBox("Desmarcar todas as tarefas");
+		quadroDeTarefas.add(selecionarTudo, 1, 0, 4, 1);
+		quadroDeTarefas.add(retirarSelecaoTudo, 5, 0, 4, 1);
+
+		ScrollBar scroll = new ScrollBar();
+		scroll.setLayoutX(scroll.getWidth()-quadroDeTarefas.getWidth());
+		scroll.setOrientation(Orientation.VERTICAL);
+		scroll.setMin(0);
+		scroll.setPrefHeight(quadroDeTarefas.USE_COMPUTED_SIZE);
+		
+
+		// Alterando a posição do scroll com o movimento do mesmo
+		scroll.valueProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				quadroDeTarefas.setLayoutY(-newValue.doubleValue());
+
+			}
+
+		});
 
 		Button selecionar = new Button("Selecionar");
 		selecionar.getStyleClass().add("botao");
@@ -276,36 +339,87 @@ public class TelaPrincipal extends Application {
 
 		selecionar.setAccessibleHelp("Concluir seleção de tarefas");
 		voltar.setAccessibleHelp("Voltar para a tela inicial");
-
+		
 		CheckBox check = new CheckBox();
 		check.getStyleClass().add("fontLabelAssitant");
 		check.getStyleClass().add(fonteGeral);
 
+		int quantidadeTarefas = tarefas.getTarefas().size();
+		// número de linhas e colunas da grid: dividir as tarefas em grupos de 2+ 1
+		// coluna para o scroll
+		int auxiliarPosicaoLinha = 1;
+		int auxiliarPosicaoColuna = 0;
+
+		for (int i = 0; i < quantidadeTarefas/2; i++) {
+			quadroDeTarefas.getRowConstraints().add(linhas());
+		}
+		//duas colunas para cada tarefa
+		quadroDeTarefas.getColumnConstraints().addAll(colunasTarefas(20), colunasTarefas(20), 
+				colunasTarefas(20), colunasTarefas(20), colunasTarefas(20), colunasTarefas(20), 
+				colunasTarefas(20), colunasTarefas(20), colunasTarefas(5));
+
 		// adicionando os nodes (elementos)
 		for (String tarefa : tarefas.getTarefas()) {
-			check = null;
-			check = inserirCombo(tarefa);
-			quadroInterno.getChildren().add(check);
-			checkboxes.add(check);
+			  check = null; 
+			  check = inserirCombo(tarefa); 
+			  quadroDeTarefas.add(check, auxiliarPosicaoColuna, auxiliarPosicaoLinha, 4, 1);
+			  checkboxes.add(check);
+			  auxiliarPosicaoColuna+=4;
+			  
+			  if(auxiliarPosicaoColuna == 8) {
+				  auxiliarPosicaoLinha++;
+				  auxiliarPosicaoColuna = 0;
+			  }
 		}
-		HBox controles = new HBox(25.0);
+		
+		quadroDeTarefas.add(scroll, 8, 0, 1, auxiliarPosicaoLinha);
+		quadroDeTarefas.setGridLinesVisible(true);
+
+		HBox controles = new HBox(100.0);
 
 		controles.getChildren().addAll(voltar, selecionar);
 		controles.setAlignment(Pos.BOTTOM_CENTER);
-		quadroInterno.getChildren().add(controles);
+		quadroDeTarefas.getRowConstraints().add(linhas());
+		quadroDeTarefas.add(controles, 0, (auxiliarPosicaoLinha+1), 8, 1);
+		// quadroInterno.getChildren().add(controles);
 
 		// Importando o css criado para minha classe:
 		String caminhoCss = getClass().getResource("/telaRobo/PrimeiroFx.css").toExternalForm();
-		quadroInterno.getStylesheets().add(caminhoCss);
+		// quadroInterno.getStylesheets().add(caminhoCss);
+		quadroDeTarefas.getStylesheets().add(caminhoCss);
+		quadroDeTarefas.getStyleClass().add("quadroDeTarefas");
 
+		//Botão voltar
 		voltar.setOnAction(e -> {
 			primaryStage.setScene(telaPrincipal);
 		});
+		
+		//selecionar tudo
+		selecionarTudo.setOnAction(e->{
+			int i = 0;
+			if(selecionarTudo.isSelected()) {
+				for(CheckBox c: checkboxes) {
+					c.setSelected(true);
+					tamanhoLista++;
+				}
+			}
+		});
+		
+		//desmarcar tudo
+		selecionarTudo.setOnAction(e->{
+			int i = 0;
+			if(retirarSelecaoTudo.isSelected()) {
+				for(CheckBox c: checkboxes) {
+					c.setSelected(false);
+				}
+				tamanhoLista=0;
+			}
+		});
 
+		//botão selecionar tarefas
 		selecionar.setOnAction(e -> {
-			int tamanhoLista = 0;
-			for(CheckBox c : checkboxes) {
-				if(c.isSelected()) {
+			for (CheckBox c : checkboxes) {
+				if (c.isSelected()) {
 					tamanhoLista++;
 				}
 			}
@@ -314,12 +428,12 @@ public class TelaPrincipal extends Application {
 			String tarefasSelecionadas = "";
 			for (CheckBox c : checkboxes) {
 				if (c.isSelected()) {
-					System.out.println("I: "+aux);
-					if (aux < tamanhoLista-1) {
-						tarefasSelecionadas = tarefasSelecionadas.concat("\r\""+c.getText()+"\" ," +"\r\n");
+					System.out.println("I: " + aux);
+					if (aux < tamanhoLista - 1) {
+						tarefasSelecionadas = tarefasSelecionadas.concat("\r\"" + c.getText() + "\" ," + "\r\n");
 						System.out.println(tarefasSelecionadas);
 					} else {
-						tarefasSelecionadas += "\r\""+c.getText()+"\"";
+						tarefasSelecionadas += "\r\"" + c.getText() + "\"";
 					}
 
 					aux++;
@@ -334,15 +448,11 @@ public class TelaPrincipal extends Application {
 							+ "		\"url\": \"https://pjeih.tjma.jus.br/pje/login.seam\",\r\n"
 							+ "		\"perfis\": [\r\n"
 							+ "				\"2ª Vara da Fazenda Pública de São Luís / Secretaria / Diretor de Secretaria\"\r\n"
-							+ "\r\n" 
-							+ "		],\r\n" 
-							+ "		\"tarefas\": [\r\n" 
-							+ "				"+tarefasSelecionadas + "\r\n" 
-							+ "		 ],\r\n"
-							+ "		\"deletarArquivo\":\"não\",\r\n" + "		\"fonteDeDados\":\"TAREFA\",\r\n"
-							+ "		\"filtrarEtiqueta\":\"1.\",\r\n" + "		\"documentosProcesso\":[\r\n"
-							+ "								\"Selecione\"\r\n" + "		],\r\n"
-							+ "		\"caminhoDownload\": \"C:/Users/wcmsodre/Downloads/\",\r\n"
+							+ "\r\n" + "		],\r\n" + "		\"tarefas\": [\r\n" + "				"
+							+ tarefasSelecionadas + "\r\n" + "		 ],\r\n" + "		\"deletarArquivo\":\"não\",\r\n"
+							+ "		\"fonteDeDados\":\"TAREFA\",\r\n" + "		\"filtrarEtiqueta\":\"1.\",\r\n"
+							+ "		\"documentosProcesso\":[\r\n" + "								\"Selecione\"\r\n"
+							+ "		],\r\n" + "		\"caminhoDownload\": \"C:/Users/wcmsodre/Downloads/\",\r\n"
 							+ "		\"usuario\":\"67324134300\",\r\n" + "		\"senha\":\"pje123456\"\r\n"
 							+ "	}\r\n" + "]");
 					br.close();
@@ -350,11 +460,12 @@ public class TelaPrincipal extends Application {
 					e1.printStackTrace();
 				}
 			}
+			
 			primaryStage.setScene(telaPrincipal);
 		});
 
 		// Preparando a exibição da tela
-		tarefaScene = new Scene(quadroInterno, 950.0, 750.0);
+		tarefaScene = new Scene(quadroDeTarefas, 950.0, 850.0);
 
 	}
 
@@ -369,4 +480,5 @@ public class TelaPrincipal extends Application {
 		return checkbox;
 
 	}
+	
 }
