6d7e6ba0296826d9f8545f27dc78c60124213600
Pedro Victor <pedro.mattis.df@gmail.com>
2022-05-05 19:01:33 -0300

    Tentando integrar o Sistema com o firebase

---
+++ src/infra/ConexaoFirebase.java
@@ -0,0 +1,20 @@
+package infra;
+/*import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+public class ConexaoFirebase {
+	
+
+	// Use the application default credentials
+	GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+	FirebaseOptions options = new FirebaseOptions.Builder()
+	    .setCredentials(credentials)
+	    .setProjectId(projectId)
+	    .build();
+	FirebaseApp.initializeApp(options);
+
+	Firestore db = FirestoreClient.getFirestore();
+}*/
